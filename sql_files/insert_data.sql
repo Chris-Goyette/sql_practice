32ccd5968db2ad86ceb0f86ce5979ba2d713e8ed
ChrisGoyette <goyettecj@gmail.com>
2021-07-28 02:17:15 -0700

    inlcudes instructions for code in comments

--- sql_files/insert_data.sql
+++ sql_files/insert_data.sql
@@ -2,6 +2,11 @@
 
 This code is how you insert data into tabels
 
+INSERT INTO <table_name> (<column_1>, <column_2>, <column_3>...)
+VALUES (<value_1>, <value_2>, <value_3>...)
+        (<value_1>, <value_2>, <value_3>...)
+        (<value_1>, <value_2>, <value_3>...)
+
 */
 
 INSERT INTO pets (pet_name, pet_type, sex, age)
